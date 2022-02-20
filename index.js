082469a7c43547e37ecc5b46ce637a1dd928e8de
= <=>
2022-02-20 15:52:28 -0500

    Configurar servidor de Express

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-console.log('hola de nuevo')
+const express = require('express')
+const res = require('express/lib/response')
+
+
+//Crear servidor aplicacion de express
+const app = express()
+
+//GET
+app.get('/', (req, res) => {
+    res.json({
+        ok: true,
+        msg: 'todo ok',
+        uid: '1234'
+    })
+})
+
+
+//Port
+app.listen(3000, () => {
+    console.log(`Servidor corriendo en puerto ${3000}`)
+})
