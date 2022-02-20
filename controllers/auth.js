d1ce450083b6f13614e816aadfc19a0d37045918
= <=>
2022-02-20 17:56:55 -0500

    Custom Middleware - ValidarCampos

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,16 +1,7 @@
 const { response } = require('express')
-const { validationResult } = require('express-validator')
 
 
 const crearUsuario = (req, res = response) => {
-
-    const errors = validationResult(req)
-    if (!errors.isEmpty()) {
-        return res.status(400).json({
-            ok: false,
-            errors: errors.mapped()
-        })
-    }
 
     const { email, name, password } = req.body
     console.log(email, name, password)
@@ -22,17 +13,8 @@ const crearUsuario = (req, res = response) => {
 
 const loginUsuario = (req, res = response) => {
 
-    const errors = validationResult(req)
-    if (!errors.isEmpty()) {
-        return res.status(400).json({
-            ok: false,
-            errors: errors.mapped()
-        })
-    }
-
 
     const { email, password } = req.body
-    console.log(email, password)
     return res.json({
         ok: 'true',
         msg: 'Login de usuario /'
--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,7 @@
 const { Router } = require('express')
 const { check } = require('express-validator')
 const { crearUsuario, loginUsuario, revalidarToken } = require('../controllers/auth')
+const { validarCampos } = require('../middlewares/validar-campos')
 
 const router = Router()
 
@@ -8,13 +9,15 @@ const router = Router()
 router.post('/new', [
     check('name', 'El nombre es obligatorio').notEmpty(),
     check('email', 'El email es obligatorio').isEmail(),
-    check('password', 'La contaseña obligatorio').isLength({ min: 6 })
+    check('password', 'La contaseña obligatorio').isLength({ min: 6 }),
+    validarCampos
 ], crearUsuario)
 
 //Login de usuario
 router.post('/', [
     check('email', 'El email es obligatorio').isEmail(),
-    check('password', 'La contaseña obligatorio').isLength({ min: 6 })
+    check('password', 'La contaseña obligatorio').isLength({ min: 6 }),
+    validarCampos
 ], loginUsuario)
 
 //validar y revalidar doken
