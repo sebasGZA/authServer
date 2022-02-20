4c5f11fa605dfb0b28ed545364e39bf1782e91c1
= <=>
2022-02-20 16:37:46 -0500

    Configurar CORS y body de las peticiones

--- controllers/auth.js
+++ controllers/auth.js
@@ -2,6 +2,8 @@ const { response } = require('express')
 
 
 const crearUsuario = (req, res = response) => {
+    const { email, name, password } = req.body
+    console.log(email, name, password)
     return res.json({
         ok: 'true',
         msg: 'crear Usuario /new'
@@ -9,6 +11,8 @@ const crearUsuario = (req, res = response) => {
 }
 
 const loginUsuario = (req, res = response) => {
+    const { email, password } = req.body
+    console.log(email, password)
     return res.json({
         ok: 'true',
         msg: 'Login de usuario /'
--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 const express = require('express')
-const res = require('express/lib/response')
+const cors = require('cors')
 
 
 //Crear servidor aplicacion de express
 const app = express()
+
+//CORS -> proteccion de backend para un dominio de front fijo
+app.use(cors())
+
+//Lectura y parseo del body
+app.use(express.json())
 
 //rutas
 app.use('/api/auth', require('./routes/auth'))
